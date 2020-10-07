ea8aa8326d9a334986f988372a6ffbe625bf34fd
aleksandrchernogorov <60900369+aleksandrchernogorov@users.noreply.github.com>
2020-10-07 22:51:25 +0500

    Fixed the changes

--- build/index.js
+++ build/index.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var sorter_1 = require("./sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 2000, -5, 0]);
-var sorter = new sorter_1.Sorter(numbersCollection);
+var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([10, 2000, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('azFc');
+var sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
-// [1, -3, 5, 10] => [-3, 1, 5, 10]
-// [a, X, f, e] => [a, e, f, X]
+console.log(charactersCollection);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { Sorter } from './sorter';
+import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 2000, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 2000, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('azFc');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
-
-// [1, -3, 5, 10] => [-3, 1, 5, 10]
-// [a, X, f, e] => [a, e, f, X]
+console.log(charactersCollection);
